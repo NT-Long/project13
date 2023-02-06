7aedf49d69057f28fcb981a78cd45bbdfafee24d
Long <107291684+NT-Long@users.noreply.github.com>
2023-02-06 20:45:06 +0700

    Create dilivery_man_by_cp.py

--- dilivery_man_by_cp.py
+++ dilivery_man_by_cp.py
@@ -1,6 +1,8 @@
 from ortools.sat.python import cp_model
 
+
 def main():
+  #input data here
   model = cp_model.CpModel()
   distance = [
     [0, 10, 75, 90, 5, 10], 
@@ -11,52 +13,56 @@ def main():
     [10, 15, 40, 45, 35, 0]
     ]
   deliver_time = [0,1,1,2,2,3]
-  num_customers=5 
-  num_vehicles=3
+  N=5 
+  K=3
   # Define the decision variables.
   route = {}
-  for k in range(num_vehicles):
-    for i in range(num_customers+1):
-      for j in range(num_customers+1):
+  for k in range(K):
+    for i in range(N+1):
+      for j in range(N+1):
         route[(k,i,j)] = model.NewBoolVar('route_%d_%d_%d' % (k,i, j))
-  for i in range(1,num_customers+1): 
-    model.Add(sum(route[(k,i,j)] for j in range(num_customers+1) for k in range(num_vehicles) if i!=j)==1)
-  for j in range(1,num_customers+1):
-    model.Add(sum(route[(k,i,j)] for i in range(num_customers+1) for k in range(num_vehicles) if i!=j)==1)
-  for k in range(num_vehicles):  
-    model.Add(sum(route[(k,0,j)] for j in range(1,num_customers+1))==1)
-  for k in range(num_vehicles):  
-    model.Add(sum(route[(k,j,0)] for j in range(1,num_customers+1))==1)
-  for k in range(num_vehicles):
-    for h in  range(1,num_customers+1):
+  #Exactly 1 vehicle goes into point [ j ] for every point [ j ] ≠ 0
+  for i in range(1,N+1): 
+    model.Add(sum(route[(k,i,j)] for j in range(N+1) for k in range(K) if i!=j)==1)
+  #Exactly 1 vehicle goes out of point [ i ] for every point [ i ] ≠ 0
+  for j in range(1,N+1):
+    model.Add(sum(route[(k,i,j)] for i in range(N+1) for k in range(K) if i!=j)==1)
+  #. All vehicles have to start at point 0
+  for k in range(K):  
+    model.Add(sum(route[(k,0,j)] for j in range(1,N+1))==1)
+  #If a vehicle go into point h ≠ 0, then it has to go out of point h
+
+  for k in range(K):
+    for h in  range(1,N+1):
       model.Add(
-        sum(route[(k,i,h)] for i in range(num_customers+1) if i!=h)
+        sum(route[(k,i,h)] for i in range(N+1) if i!=h)
         -
-        sum(route[(k,h,j)] for j in range(num_customers+1) if j!=h)
+        sum(route[(k,h,j)] for j in range(N+1) if j!=h)
         ==0
       )
 
     
-
-  for h in range(1,num_customers+1):
-    vehicle_in=sum(route[(k,h,j)] for j in range(1,num_customers+1) for k in range(num_vehicles) if h!=j)
+  #all customers visited exactly one
+  for h in range(1,N+1):
+    vehicle_in=sum(route[(k,h,j)] for j in range(1,N+1) for k in range(K) if h!=j)
     
-    vehicle_out=sum(route[(k,j,h)] for j in range(1,num_customers+1) for k in range(num_vehicles) if h!=j)
+    vehicle_out=sum(route[(k,j,h)] for j in range(1,N+1) for k in range(K) if h!=j)
     
     model.Add(vehicle_in+vehicle_out<=1)
 
-  
-  obj=sum(distance[i][j]*route[(k,i,j)] for i in range(num_customers+1) for j in range(num_customers+1) for k in range(num_vehicles) if j!=0)
+  #object fuction
+  obj=sum(distance[i][j]*route[(k,i,j)] for i in range(N+1) for j in range(N+1) for k in range(K) if j!=0)
   model.Minimize(obj)
-  
+  #solve
   solver = cp_model.CpSolver()
   status = solver.Solve(model)
+  #print method
   if status == cp_model.OPTIMAL:
     print('Minimal total distance: %d' % (solver.ObjectiveValue()+sum(deliver_time)))
-    for k in range(num_vehicles):
+    for k in range(K):
         customers_visited = []
-        for i in range(num_customers+1):
-          for j in range(num_customers+1):
+        for i in range(N+1):
+          for j in range(N+1):
             if solver.Value(route[k,i,j]) == 1 and j!=0 :
               if i not in customers_visited:    
                 customers_visited.append(i)
