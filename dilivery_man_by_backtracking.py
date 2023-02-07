361ef51a525cc93f38d63ab2a2668ab2984a47a0
Long <107291684+NT-Long@users.noreply.github.com>
2023-02-07 20:27:21 +0700

    Update dilivery_man_by_backtracking.py

--- dilivery_man_by_backtracking.py
+++ dilivery_man_by_backtracking.py
@@ -1,15 +1,13 @@
 # input information here
-deliver_time = [0,1,1,2,2,3]
-distance = [
-    [0, 10, 75, 90, 5, 10], 
+N =5
+K =3
+deliver_time = [2,3,1,2,2,3,3,1,2]
+distance = [[0, 10, 75, 90, 5, 10], 
     [10, 0, 100, 25, 40,15],
     [75, 100, 0, 30, 25,40], 
     [90, 25, 30, 0, 30,45],
     [5, 40, 25, 30, 0, 35],
-    [10, 15, 40, 45, 35, 0]
-]
-N=5
-K=3
+    [10, 15, 40, 45, 35, 0]]
 N=[x for x in range(1,N+1)]
 K=[x for x in range(1,K+1)]
 n=len(N)
@@ -18,7 +16,7 @@ answer={}
 for i in range(n):
     for i1 in range(len(K)):
         answer[i]=i1
-note=[]
+
 def permutation(lst):
 
 	if len(lst) == 0:
@@ -35,22 +33,8 @@ def permutation(lst):
 	    for p in permutation(remLst):
 		    l.append([m] + p)
 	return l
-#using backtracking to listed all posible outcomes and then save it into note
-def posible(N,K,answer,m):
-    global note
-    if m==len(N):
-        a=answer[::1]
-        note.append(a)
-        return 
-    
-    for i in K:
-        answer[m]=i
-        posible(N,K,answer,m+1)
-posible(N,K,to_writte,0)
-result=float('inf')
-opt=float('inf')
-#check each outcome
-for i in note:
+#Try function to check each outcome
+def Try(i):
     last_move=[]
     for k in K:
         x=[]
@@ -72,13 +56,34 @@ for i in note:
                 cost=cost+distance[path[way]][path[way+1]]
             optCost=min(cost,optCost) 
         pathcost+=optCost
-    #save best possibility of each iteration
-    if pathcost<result:
-        result=pathcost
-        opt=last_move
+    return pathcost,last_move
+result=float('inf')
+opt=float('inf')
+#using backtracking to try all possible outcomes
+def possible(N,K,answer,m):
+    global result,opt
+    if m==len(N):
+        
+        a=answer[::1]
+        pathcost,last_move= Try(a)
+        if pathcost<result:
+            result=pathcost
+            opt=last_move
+
+        return 
+    
+    for i in K:
+        answer[m]=i
+        possible(N,K,answer,m+1)
+
+possible(N,K,to_writte,0)
+
+#check each outcome
+
+    
 print('Minimal solution result: ',result+sum(deliver_time))
 final=[]
-#print minimal path of each delivery mans
+
 o=0
 for delivery in opt:
         m=[]
